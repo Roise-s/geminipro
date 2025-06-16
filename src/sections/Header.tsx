06c3301ba925ff3efca2af406bfab2f2d7f67bb1
Kgaugelo Tladi <163626679+Roise-s@users.noreply.github.com>
2025-06-16 18:25:29 +0200

    Update Header.tsx

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -3,7 +3,7 @@ const Header = () => {
   return (
       <header className="py-6 px-4 md:px-8 bg-[#1f1f1f] shadow flex items-center gap-2">
         <img src="/logo.png" alt="Gemini" className="w-8 h-8" />
-        <h1 className="text-2xl font-bold text-amber-50">Gemini Pro</h1>
+        <h1 className="text-2xl font-bold text-amber-50">Kasi AI</h1>
       </header>
   )
 }
